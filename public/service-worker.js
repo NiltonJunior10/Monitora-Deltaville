9139f5f132380aab6726ff707a99d2ad62c7014f
Nilton Santos Junior <niltonsantosjr@icloud.com>
2026-09-21 09:17:20 -0300

    chore: refresh cache for occurrence icon alignment

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="monitora-deltaville-v702-ios27";
+const CACHE="monitora-deltaville-v703-ios27";
 const APP_SHELL=[
   "https://unpkg.com/@supabase/supabase-js@2.113.0",
   "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js",
@@ -8,7 +8,7 @@ const APP_SHELL=[
   "./styles.css?v=702",
   "./stability.css?v=702",
   "./apple-ui.css?v=702",
-  "./v6.css?v=702",
+  "./v6.css?v=703",
   "./auth.js?v=702",
   "./river.js?v=702",
   "./weather.js?v=702",
