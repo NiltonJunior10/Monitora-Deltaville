df49008b36dad710e54307f2cb3d4fde5b716f34
Nilton Santos Junior <niltonsantosjr@icloud.com>
2026-09-21 19:25:24 -0300

    Atualiza cache do select de ocorrencia v7.0.51

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="monitora-deltaville-v750-ios27";
+const CACHE="monitora-deltaville-v751-ios27";
 const APP_SHELL=[
   "https://unpkg.com/@supabase/supabase-js@2.113.0",
   "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js",
@@ -9,7 +9,7 @@ const APP_SHELL=[
   "./stability.css?v=702",
   "./apple-ui.css?v=702",
   "./v6.css?v=704",
-  "./v8.css?v=748",
+  "./v8.css?v=751",
   "./river-desktop.css?v=720",
   "./sidebar-desktop.css?v=725",
   "./auth.js?v=747",
