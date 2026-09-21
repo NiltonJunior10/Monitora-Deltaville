11865ff1a12b07a5bb96cbb4295f4dc0577bda62
Nilton Santos Junior <niltonsantosjr@icloud.com>
2026-09-21 16:41:06 -0300

    Atualiza cache do card do Rio v7.0.33

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="monitora-deltaville-v732-ios27";
+const CACHE="monitora-deltaville-v733-ios27";
 const APP_SHELL=[
   "https://unpkg.com/@supabase/supabase-js@2.113.0",
   "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js",
@@ -9,7 +9,7 @@ const APP_SHELL=[
   "./stability.css?v=702",
   "./apple-ui.css?v=702",
   "./v6.css?v=704",
-  "./v8.css?v=732",
+  "./v8.css?v=733",
   "./river-desktop.css?v=720",
   "./sidebar-desktop.css?v=725",
   "./auth.js?v=702",
