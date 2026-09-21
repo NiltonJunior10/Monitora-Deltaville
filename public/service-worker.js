b1c10bd6a8d7bbaa661d686538695f3b820c27fa
Nilton Santos Junior <niltonsantosjr@icloud.com>
2026-09-21 19:01:42 -0300

    Atualiza cache do acesso centralizado v7.0.46

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="monitora-deltaville-v745-ios27";
+const CACHE="monitora-deltaville-v746-ios27";
 const APP_SHELL=[
   "https://unpkg.com/@supabase/supabase-js@2.113.0",
   "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js",
@@ -9,7 +9,7 @@ const APP_SHELL=[
   "./stability.css?v=702",
   "./apple-ui.css?v=702",
   "./v6.css?v=704",
-  "./v8.css?v=745",
+  "./v8.css?v=746",
   "./river-desktop.css?v=720",
   "./sidebar-desktop.css?v=725",
   "./auth.js?v=702",
