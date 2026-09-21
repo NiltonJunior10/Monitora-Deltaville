270a7e1aed8cf5d8e90c91a6d608bca45b491de5
Nilton Santos Junior <niltonsantosjr@icloud.com>
2026-09-21 16:25:08 -0300

    Atualiza cache da permissão administrativa v7.0.27

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="monitora-deltaville-v725-ios27";
+const CACHE="monitora-deltaville-v727-ios27";
 const APP_SHELL=[
   "https://unpkg.com/@supabase/supabase-js@2.113.0",
   "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js",
@@ -16,7 +16,7 @@ const APP_SHELL=[
   "./river.js?v=720",
   "./weather.js?v=702",
   "./push.js?v=702",
-  "./app.js?v=712",
+  "./app.js?v=713",
   "./v6-ui.js?v=707",
   "./v8-ux.js?v=704",
   "./stability-core.js?v=702",
