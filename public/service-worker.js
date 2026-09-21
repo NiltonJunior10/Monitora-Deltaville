abc0e8eb3254480881834e001a57671d5d703901
Nilton Santos Junior <niltonsantosjr@icloud.com>
2026-09-21 16:36:50 -0300

    Atualiza cache da régua do Rio v7.0.31

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="monitora-deltaville-v730-ios27";
+const CACHE="monitora-deltaville-v731-ios27";
 const APP_SHELL=[
   "https://unpkg.com/@supabase/supabase-js@2.113.0",
   "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js",
@@ -9,11 +9,11 @@ const APP_SHELL=[
   "./stability.css?v=702",
   "./apple-ui.css?v=702",
   "./v6.css?v=704",
-  "./v8.css?v=730",
+  "./v8.css?v=731",
   "./river-desktop.css?v=720",
   "./sidebar-desktop.css?v=725",
   "./auth.js?v=702",
-  "./river.js?v=729",
+  "./river.js?v=731",
   "./weather.js?v=702",
   "./push.js?v=702",
   "./app.js?v=713",
